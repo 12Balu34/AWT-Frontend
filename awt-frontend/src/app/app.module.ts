b929e57429e8301c56ad8b89f58c66cb9729cf5c
12Balu34 <bastian.scherber@gmail.com>
2018-07-09 01:20:03 +0200

    Added html for signup added routes

--- awt-frontend/src/app/app.module.ts
+++ awt-frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
 import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    MainComponent
+    MainComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
