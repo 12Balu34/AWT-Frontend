6f44b293b197313221993d4599967f34b994a524
12Balu34 <bastian.scherber@gmail.com>
2018-07-11 22:39:36 +0200

    Added route guard for homepage

--- awt-frontend/src/app/app-routing.module.ts
+++ awt-frontend/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import {RouterModule, Routes} from "@angular/router";
 import {LoginComponent} from "./login/login.component";
 import {MainComponent} from "./main/main.component";
 import {SignupComponent} from "./signup/signup.component";
+import {AuthGuardService} from "./services/authguard/auth-guard.service";
 
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent},
-  { path: 'home', component: MainComponent },
+  { path: 'home', component: MainComponent,  canActivate: [AuthGuardService] },
   { path: '',   redirectTo: '/login', pathMatch: 'full' }
 ];
 
