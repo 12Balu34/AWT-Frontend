e13cb58926b59a9553cbacedebbbcbe4cb7a346f
12Balu34 <bastian.scherber@gmail.com>
2018-07-12 12:30:33 +0200

    Moved component

--- awt-frontend/src/app/views/main/create-campaign/create-campaign.component.spec.ts
+++ awt-frontend/src/app/views/main/create-campaign/create-campaign.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { CreateCampaignComponent } from './create-campaign.component';
+import {CreateCampaignComponent} from './create-campaign.component';
 
 describe('CreateCampaignComponent', () => {
   let component: CreateCampaignComponent;
