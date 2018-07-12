6242215765adc5281bd7597e16802df83880d8ad
12Balu34 <bastian.scherber@gmail.com>
2018-07-12 12:28:33 +0200

    Added new component

--- awt-frontend/src/app/app.module.ts
+++ awt-frontend/src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
-import { LoginComponent } from './views/login/login.component';
-import { MainComponent } from './views/main/main.component';
-import { SignupComponent } from './views/signup/signup.component';
+import {LoginComponent} from './views/login/login.component';
+import {MainComponent} from './views/main/main.component';
+import {SignupComponent} from './views/signup/signup.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {httpInterceptorProviders} from "./http-interceptors";
 import {AuthService} from "./services/auth/auth.service";
 import {HttpClientModule} from "@angular/common/http";
-import { CreateCampaignComponent } from './views/main/create-campaign/create-campaign/create-campaign.component';
+import {CreateCampaignComponent} from './views/main/create-campaign/create-campaign.component';
+import {EditUserComponent} from './views/main/edit-user/edit-user.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { CreateCampaignComponent } from './views/main/create-campaign/create-cam
     LoginComponent,
     MainComponent,
     SignupComponent,
-    CreateCampaignComponent
+    CreateCampaignComponent,
+    EditUserComponent
   ],
   imports: [
     BrowserModule,
