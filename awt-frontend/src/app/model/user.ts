67ece62ea73abcbf0ee93bf29893bb0315947746
12Balu34 <bastian.scherber@gmail.com>
2018-07-12 12:19:26 +0200

    Changed interface to class, added constructor

--- awt-frontend/src/app/model/user.ts
+++ awt-frontend/src/app/model/user.ts
@@ -1,6 +1,14 @@
-export interface User {
+export class User {
   username: string;
   email: string;
   password: string;
   role: string;
+
+
+  constructor(username: string, email: string, password: string, role: string) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.role = role;
+  }
 }
