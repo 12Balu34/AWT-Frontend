f64d44339a25d47fb94e2f2449077d351e931bc8
12Balu34 <bastian.scherber@gmail.com>
2018-07-18 21:30:43 +0200

    Added routes for new components

--- awt-frontend/src/app/app-routing.module.ts
+++ awt-frontend/src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import {MainComponent} from "./main/main.component";
 import {SignupComponent} from "./views/signup/signup.component";
 import {AuthGuardService} from "./services/authguard/auth-guard.service";
 import {EditUserComponent} from "./main/edit-user/edit-user.component";
+import {CreateCampaignComponent} from "./main/create-campaign/create-campaign.component";
+import {ManagedCampaignsComponent} from "./main/managed-campaigns/managed-campaigns.component";
+import {ManagerAuthguardService} from "./services/manager-auth/manager-authguard.service";
 
 
 const appRoutes: Routes = [
@@ -12,7 +15,9 @@ const appRoutes: Routes = [
   { path: 'signup', component: SignupComponent},
   { path: 'home', component: MainComponent,  canActivate: [AuthGuardService],
     children: [
-      { path: 'profile', component: EditUserComponent,  canActivate: [AuthGuardService]}
+      { path: 'profile', component: EditUserComponent,  canActivate: [AuthGuardService]},
+      { path: 'create-campaign', component: CreateCampaignComponent,  canActivate: [AuthGuardService, ManagerAuthguardService]},
+      { path: 'campaigns', component: ManagedCampaignsComponent,  canActivate: [AuthGuardService]},
     ] },
   { path: '',   redirectTo: '/login', pathMatch: 'full' }
 ];
