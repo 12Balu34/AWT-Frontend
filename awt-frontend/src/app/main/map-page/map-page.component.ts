55ecf9d8dfb1018c810d380ec960f289563d1b66
12Balu34 <bastian.scherber@gmail.com>
2018-07-22 11:05:59 +0200

    Added functionality

--- awt-frontend/src/app/main/map-page/map-page.component.ts
+++ awt-frontend/src/app/main/map-page/map-page.component.ts
@@ -1,12 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import * as L from 'leaflet';
-import {icon, Map, marker, tileLayer} from 'leaflet';
+import {icon, Map, tileLayer} from 'leaflet';
 import {Peak} from "../../model/Peak";
 import {MarkerColors} from "../../model/MarkerColors";
 import {ActivatedRoute, Router} from "@angular/router";
 import {PeakService} from "../../services/peak/peak.service";
 import {Observable} from "rxjs/internal/Observable";
-import {Subject} from "rxjs/internal/Subject";
+import {TSMap} from "typescript-map";
 
 @Component({
   selector: 'app-map-page',
@@ -18,10 +18,12 @@ export class MapPageComponent implements OnInit {
   private peakList: Peak [];
   private baselayer = tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: 'Open Street Map' });
   map: L.Map;
+  selectedPeak: Peak
+  mapMarkers = new TSMap<string, L.Marker>()
 
 
   options = {
-    layers: [],
+    layers: [this.baselayer],
     zoom: 5,
     center: L.latLng([ 47, 8 ])
   };
@@ -29,73 +31,63 @@ export class MapPageComponent implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
-    private peakService: PeakService
-  ) {
-    this.eventSubject
-      .subscribe((v) => { console.log(v) } );
-  }
+    private peakService: PeakService,
+  ) {}
 
   ngOnInit() {
+
+  }
+  onMapReady(map: Map) {
+    this.map = map;
     this.getPeakListObservable();
     this.subscribeToPeakListObservable();
   }
 
   addAllMarkers() {
-    console.log('accessed addAllMarkers');
-    this.options.layers.push(this.baselayer);
-
     for (let peak of this.peakList) {
       this.addSingleMarker(peak);
     }
   }
 
   addSingleMarker(peak: Peak) {
-    console.log('accessed addSingleMarker');
-    this.options.layers.push(
-      this.markerBuilder(peak)
-    );
-  }
-
-  markerBuilder(peak: Peak){
-    console.log('accessed markerBuilder');
     let color = this.resolveMarkerColor(peak);
-    return marker(
+
+    let marker = L.marker(
       [peak.latitude, peak.longitude],
       {
-        icon: this.iconBuilder(color),
-        title: peak.id.toString(),
-        clickable: true
-      }
-    )
+        icon: icon({
+        iconUrl: 'assets/marker-icon-' + color + '.png',
+        shadowUrl: 'assets/marker-shadow.png'
+      }),
+      title: peak.id.toString(),
+      clickable: true
+      })
+      .on('click', this.onMarkerClick, this)
+      .addTo(this.map)
+
+      this.mapMarkers.set(peak.id.toString(), marker);
   }
 
   private resolveMarkerColor(peak: Peak): MarkerColors {
-    console.log('accessed resolveMarkerColor');
     let annotationsLenght: number = peak.annotations.length;
 
     if(!peak.toBeAnnotated) {
-      console.log('returned green');
       return MarkerColors.Green;
     }
     if(annotationsLenght == 0 && peak.toBeAnnotated) {
-      console.log('returned yellow');
       return MarkerColors.Yellow;
     }
     if(annotationsLenght > 0 && peak.toBeAnnotated) {
-      console.log('returned orange');
       return MarkerColors.Orange;
     }
     if (peak.hasConflict()) {
-      console.log('returned red');
       return MarkerColors.Red;
     }
-    console.log('returned green');
     return MarkerColors.Blue;
   }
 
 
-  iconBuilder(color: MarkerColors) {
-    console.log('accessed iconBuilder')
+  iconBuilder(color: MarkerColors): L.Icon {
     return icon({
       iconUrl: 'assets/marker-icon-' + color + '.png',
       shadowUrl: 'assets/marker-shadow.png'
@@ -114,7 +106,6 @@ export class MapPageComponent implements OnInit {
   private subscribeToPeakListObservable() {
     this.peakListObservable.subscribe(
       data => {
-        console.log('Received data with length: ' + data.length)
         this.peakList = data;
         this.addAllMarkers();
       },
@@ -124,16 +115,15 @@ export class MapPageComponent implements OnInit {
     )
   }
 
-  eventSubject = new Subject<string>();
-
-  onMapReady(map: Map) {
-    this.map = map;
-    L.marker([51.5, -0.09]).addTo(this.map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();  }
-
-  onMapClick(e) {
-    console.log(e.marker.title)
+  onMarkerClick(event) {
+    this.selectCurrentPeak(+event.sourceTarget.options.title);
   }
 
+  selectCurrentPeak(peakId: number){
+    for(let peak of this.peakList) {
+      if (peak.id == peakId) {
+        this.selectedPeak = peak;
+      }
+    }
+  }
 }
