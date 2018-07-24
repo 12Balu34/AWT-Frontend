8d93202023efff438b687b7d1c0779f428650434
12Balu34 <bastian.scherber@gmail.com>
2018-07-24 12:57:38 +0200

    Refactored code

--- awt-frontend/src/app/main/map-page/map-page.component.ts
+++ awt-frontend/src/app/main/map-page/map-page.component.ts
@@ -61,6 +61,7 @@ export class MapPageComponent implements OnInit {
     this.createForm();
   }
 
+  //--------- Methods related to the Annotation Form ----------
   private createForm(){
     this.annotationForm = this.formBuilder.group({
       valid: [true, Validators.required],
@@ -71,13 +72,13 @@ export class MapPageComponent implements OnInit {
     this.localizedNames = this.annotationForm.get('localizedNames') as FormArray;
   }
 
-
   private createLocalizedName(): FormGroup {
     return this.formBuilder.group({
       name: ['', Validators.required],
       language: ['', Validators.required]
     })
   }
+
   onMapReady(map: Map) {
     this.map = map;
     this.getPeakListObservable();
@@ -106,85 +107,12 @@ export class MapPageComponent implements OnInit {
       this.mapMarkers.set(peak.id.toString(), marker);
   }
 
-  updateRejectedMarkerColor(peakId: string) {
-    this.mapMarkers.get(peakId)
-      .setIcon(
-        this.iconBuilder(MarkerColors.Red)
-      );
-  }
-  updateAnnotatedMarkerColor(peakId: string) {
-    this.mapMarkers.get(peakId)
-      .setIcon(
-        this.iconBuilder(MarkerColors.Orange)
-      );
-  }
-
-  private resolveMarkerColor(peak: Peak): MarkerColors {
-    let annotationsLenght: number = peak.annotations.length;
-
-    if(!peak.toBeAnnotated) {
-      return MarkerColors.Green;
-    }
-    if(annotationsLenght == 0 && peak.toBeAnnotated) {
-      return MarkerColors.Yellow;
-    }
-    if(annotationsLenght > 0 && peak.toBeAnnotated) {
-      if(this.hasRejectedAnnotations(peak)) {
-        return MarkerColors.Red;
-      }
-      else return MarkerColors.Orange;
-    }
-    return MarkerColors.Blue;
-  }
-
-
-  iconBuilder(color: MarkerColors): L.Icon {
-    return icon({
-      iconUrl: 'assets/marker-icon-' + color + '.png',
-      shadowUrl: 'assets/marker-shadow.png'
-    })
-  }
-
-
-  private getPeakListObservable(){
-    this.activatedRoute.paramMap.subscribe(
-      data=> {
-        this.campaignId = data.get('id');
-        this.peakListObservable = this.peakService.getAllPeaks(this.campaignId);
-      }
-    )
-  }
-
-  private subscribeToPeakListObservable() {
-    this.peakListObservable.subscribe(
-      data => {
-        this.peakList = data;
-        this.addAllMarkers();
-      },
-      error => {
-        console.log(error)
-      }
-    )
-  }
-
-  onMarkerClick(event) {
-    this.selectCurrentPeak(+event.sourceTarget.options.title);
-  }
-
-  selectCurrentPeak(peakId: number){
-    for(let peak of this.peakList) {
-      if (peak.id == peakId) {
-        this.selectedPeak = peak;
-      }
-    }
-  }
-
   rejectAnnotation(peakId: number, annotationId: number) {
     this.updateRejectedMarkerColor(peakId.toString());
     //TODO: post change to backend
   }
 
-  public hasRejectedAnnotations(peak: Peak): boolean {
+  hasRejectedAnnotations(peak: Peak): boolean {
     if (peak.annotations.length == 0) {
       return false;
     }
@@ -199,8 +127,7 @@ export class MapPageComponent implements OnInit {
     return false;
   }
 
-
-  private addLocalizedName() {
+  addLocalizedName() {
     this.localizedNames.push(this.createLocalizedName());
     this.convertLocalizedPeaksToStringArray();
   }
@@ -248,6 +175,84 @@ export class MapPageComponent implements OnInit {
       )
   }
 
+
+  //--------- Methods for the Map and Markers -------------
+  updateRejectedMarkerColor(peakId: string) {
+    this.mapMarkers.get(peakId)
+      .setIcon(
+        this.iconBuilder(MarkerColors.Red)
+      );
+  }
+
+  updateAnnotatedMarkerColor(peakId: string) {
+    this.mapMarkers.get(peakId)
+      .setIcon(
+        this.iconBuilder(MarkerColors.Orange)
+      );
+  }
+
+  resolveMarkerColor(peak: Peak): MarkerColors {
+    let annotationsLenght: number = peak.annotations.length;
+
+    if(!peak.toBeAnnotated) {
+      return MarkerColors.Green;
+    }
+    if(annotationsLenght == 0 && peak.toBeAnnotated) {
+      return MarkerColors.Yellow;
+    }
+    if(annotationsLenght > 0 && peak.toBeAnnotated) {
+      if(this.hasRejectedAnnotations(peak)) {
+        return MarkerColors.Red;
+      }
+      else return MarkerColors.Orange;
+    }
+    return MarkerColors.Blue;
+  }
+
+  iconBuilder(color: MarkerColors): L.Icon {
+    return icon({
+      iconUrl: 'assets/marker-icon-' + color + '.png',
+      shadowUrl: 'assets/marker-shadow.png'
+    })
+  }
+
+  onMarkerClick(event) {
+    this.selectCurrentPeak(+event.sourceTarget.options.title);
+  }
+
+  selectCurrentPeak(peakId: number){
+    for(let peak of this.peakList) {
+      if (peak.id == peakId) {
+        this.selectedPeak = peak;
+      }
+    }
+  }
+
+
+  //--------- Methods to get Peaks -------------
+  private getPeakListObservable(){
+    this.activatedRoute.paramMap.subscribe(
+      data=> {
+        this.campaignId = data.get('id');
+        this.peakListObservable = this.peakService.getAllPeaks(this.campaignId);
+      }
+    )
+  }
+
+  private subscribeToPeakListObservable() {
+    this.peakListObservable.subscribe(
+      data => {
+        this.peakList = data;
+        this.addAllMarkers();
+      },
+      error => {
+        console.log(error)
+      }
+    )
+  }
+
+
+  //--------- Utility method to hide reset and hide messages -------------
   private resetMessage() {
     this.messageClass = null;
     this.message = null;
