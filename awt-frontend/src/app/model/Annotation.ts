066058d6159a9ed005215b8d71a7f11242c98394
12Balu34 <bastian.scherber@gmail.com>
2018-07-24 12:38:48 +0200

    Refactored

--- awt-frontend/src/app/model/Annotation.ts
+++ awt-frontend/src/app/model/Annotation.ts
@@ -1,13 +1,22 @@
 import {MinimalUser} from "./MinimalUser";
 
-export class Annotation {
+export class AnnotationBase {
+  name: string;
+  elevation: number;
+  localized_names: Array<string[]>;
+  valid: boolean;
 
+
+  constructor(name: string, elevation: number, localized_names: Array<string[]>, valid: boolean) {
+    this.name = name;
+    this.elevation = elevation;
+    this.localized_names = localized_names;
+    this.valid = valid;
+  }
+}
+  export class Annotation extends AnnotationBase{
   id: number;
   creationDateTime: string;
-  elevation: number;
-  name: string;
   worker: MinimalUser;
-  valid: boolean;
   acceptedByManager: boolean;
-  localized_names:  Array<string[]>;
 }
