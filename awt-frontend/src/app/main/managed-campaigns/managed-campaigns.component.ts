6d28c6cd3343c990c4f415997f1c9e99f9fe2912
12Balu34 <bastian.scherber@gmail.com>
2018-07-24 12:39:52 +0200

    Minor changes due to layout

--- awt-frontend/src/app/main/managed-campaigns/managed-campaigns.component.ts
+++ awt-frontend/src/app/main/managed-campaigns/managed-campaigns.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {CampaignService} from "../../services/campaign/campaign.service";
 import {Campaign} from "../../model/campaign";
 import {AuthService} from "../../services/auth/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-managed-campaigns',
@@ -11,13 +12,23 @@ import {AuthService} from "../../services/auth/auth.service";
 export class ManagedCampaignsComponent implements OnInit {
   isManager: boolean;
   campaigns: Campaign[];
+  enrolledCampaigns: Campaign[];
+  enrollMessage: string;
+  enrollMessageClass: string;
 
-  constructor(private campaignService: CampaignService, private authService: AuthService) {
+  constructor(private campaignService: CampaignService, private authService: AuthService, private router: Router) {
   }
 
   ngOnInit() {
     setTimeout(()=>this.initializeComponent(), 150)
+  }
 
+  private initializeComponent() {
+    this.getAllCampaigns();
+    this.isManager = this.authService.isManager();
+    if(!this.isManager) {
+      this.getEnrolledCampaigns();
+    }
   }
 
   private getAllCampaigns() {
@@ -25,12 +36,35 @@ export class ManagedCampaignsComponent implements OnInit {
       .subscribe(
         data => {
           this.campaigns = data;
+          console.log(this.campaigns);
         }
       )
   }
 
-  private initializeComponent() {
-    this.getAllCampaigns();
-    this.isManager = this.authService.isManager();
+  private getEnrolledCampaigns() {
+    this.campaignService.getEnrolledCampaigns()
+      .subscribe(
+        data => {
+          this.enrolledCampaigns = data;
+          console.log(this.enrolledCampaigns);
+        }
+      )
+  }
+
+  private enrolInCampaign(campaignId: number) {
+    this.campaignService.enrollInCampaign(campaignId)
+      .subscribe(
+        data => {
+          this.enrollMessage = data.message;
+          this.enrollMessageClass = 'alert alert-success';
+          setTimeout(()=>{
+            this.router.navigateByUrl('/campaigns/'+ campaignId +'/map')
+            }, 1000)
+        },
+        error => {
+          this.enrollMessage = error.error.message;
+          this.enrollMessageClass = 'alert alert-danger';
+        }
+      );
   }
 }
