13d7b324598a40e99c46d3e64a0eb20f13352ee4
12Balu34 <bastian.scherber@gmail.com>
2018-07-29 22:06:44 +0200

    Initial implementation of campaign statistics page

--- awt-frontend/src/app/main/campaign-statistics/campaign-statistics.component.ts
+++ awt-frontend/src/app/main/campaign-statistics/campaign-statistics.component.ts
@@ -1,4 +1,10 @@
 import {Component, OnInit} from '@angular/core';
+import {CampaignStatistics} from "../../model/CampaignStatistics";
+import {TSMap} from "typescript-map";
+import {Annotation} from "../../model/Annotation";
+import {ActivatedRoute} from "@angular/router";
+import {CampaignService} from "../../services/campaign/campaign.service";
+import {Observable} from "rxjs/internal/Observable";
 
 @Component({
   selector: 'app-campaign-statistics',
@@ -6,10 +12,41 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./campaign-statistics.component.css']
 })
 export class CampaignStatisticsComponent implements OnInit {
+  private campaignId: number;
+  private campaignStatistics: CampaignStatistics;
+  private campaignStatisticsObservable: Observable <CampaignStatistics>;
+  private annotatedPeaks: TSMap<number, Annotation> = new TSMap<number, Annotation>();
+  private annotatedPeaksWithRejectedAnnotations: TSMap<number, Annotation> = new TSMap<number, Annotation>();
 
-  constructor() { }
+  private message: string;
+  private messageClass: string;
+
+  constructor(private activatedRoute: ActivatedRoute, private campaignService: CampaignService) { }
 
   ngOnInit() {
+    this.getCampaignStatisticsObservable();
+    this.subscribeToCampaigStatisticsObservable();
   }
 
+  private getCampaignStatisticsObservable(){
+    this.activatedRoute.paramMap.subscribe(
+      data=> {
+        this.campaignId = +data.get('id');
+        this.campaignStatisticsObservable = this.campaignService.getCampaignStatistics(this.campaignId);
+      }
+    )
+  }
+
+  private subscribeToCampaigStatisticsObservable(){
+    this.campaignStatisticsObservable
+      .subscribe(
+        data => {
+          this.campaignStatistics = data;
+        },
+        error=> {
+          this.message = error;
+          this.messageClass = 'alert alert-danger';
+        }
+      )
+  }
 }
