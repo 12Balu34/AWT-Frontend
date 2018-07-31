2e709794367309eda72723e30fc7a756cd52c9f3
12Balu34 <bastian.scherber@gmail.com>
2018-07-31 15:11:24 +0200

    Added functionality  to view details

--- awt-frontend/src/app/main/campaign-statistics/campaign-statistics.component.ts
+++ awt-frontend/src/app/main/campaign-statistics/campaign-statistics.component.ts
@@ -5,6 +5,7 @@ import {Annotation} from "../../model/Annotation";
 import {ActivatedRoute} from "@angular/router";
 import {CampaignService} from "../../services/campaign/campaign.service";
 import {Observable} from "rxjs/internal/Observable";
+import {AnnotationService} from "../../services/annotation/annotation.service";
 
 @Component({
   selector: 'app-campaign-statistics',
@@ -15,18 +16,21 @@ export class CampaignStatisticsComponent implements OnInit {
   campaignId: number;
   campaignStatistics: CampaignStatistics;
   campaignStatisticsObservable: Observable<CampaignStatistics>;
-  annotatedPeaks: TSMap<number, Annotation> = new TSMap<number, Annotation>();
-  annotatedPeaksWithRejectedAnnotations: TSMap<number, Annotation> = new TSMap<number, Annotation>();
+
+  selectedPeakAnnotations: Annotation[];
+  selectedPeakWithRejectedAnnotations: Annotation[];
+  annotatedPeaksMap: TSMap<number, Annotation[]> = new TSMap<number, Annotation[]>();
+  annotatedPeaksWithRejectedAnnotationsMap: TSMap<number, Annotation[]> = new TSMap<number, Annotation[]>();
 
   message: string;
   messageClass: string;
 
-  constructor(private activatedRoute: ActivatedRoute, private campaignService: CampaignService) {
+  constructor(private activatedRoute: ActivatedRoute, private campaignService: CampaignService, private annotationService: AnnotationService) {
   }
 
   ngOnInit() {
     this.getCampaignStatisticsObservable();
-    this.subscribeToCampaigStatisticsObservable();
+    this.subscribeToCampaignStatisticsObservable();
   }
 
   private getCampaignStatisticsObservable() {
@@ -38,7 +42,7 @@ export class CampaignStatisticsComponent implements OnInit {
     )
   }
 
-  private subscribeToCampaigStatisticsObservable() {
+  private subscribeToCampaignStatisticsObservable() {
     this.campaignStatisticsObservable
       .subscribe(
         data => {
@@ -49,5 +53,56 @@ export class CampaignStatisticsComponent implements OnInit {
           this.messageClass = 'alert alert-danger';
         }
       )
+  }
+
+  displayAnnotations(peakId: number) {
+    //Look if peak is already in Map
+    if (this.annotatedPeaksMap.has(peakId)) {
+      this.selectedPeakAnnotations = this.annotatedPeaksMap.get(peakId);
+      return;
+    }
+    //if it was not in the Map, then get the peak and save it in the Map
+
+    this.getAllAnnotationsForPeak(peakId)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.selectedPeakAnnotations = data;
+          this.annotatedPeaksMap.set(peakId, data);
+        },
+        error => {
+          console.log(error);
+        }
+      )
+
+  }
+
+  displayRejectedAnnotations(peakId: number) {
+    //Look if peak is already in Map
+    this.selectedPeakWithRejectedAnnotations = this.annotatedPeaksWithRejectedAnnotationsMap.get(peakId);
+
+    //if it was not in the Map, then get the peak and save it in the Map
+    if (this.selectedPeakWithRejectedAnnotations == null) {
+      this.getRejectedAnnotationsForPeak(peakId)
+        .subscribe(
+          data => {
+            this.selectedPeakWithRejectedAnnotations = data;
+            this.annotatedPeaksWithRejectedAnnotationsMap.set(peakId, data)
+          }
+          ,
+          error => {
+            console.log(error);
+          }
+        )
+    }
+  }
+
+  getAllAnnotationsForPeak(peakId: number): Observable<Annotation[]> {
+    return this.annotationService.getAllAnnotationsForPeak(this.campaignId, peakId);
+  }
+
+  private getRejectedAnnotationsForPeak(peakId: number) {
+    return this.annotationService.getRejectedAnnotationsForPeak(this.campaignId, peakId);
+
   }
 }
